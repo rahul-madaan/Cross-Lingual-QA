22a4791557b206c72410adadacde0a39725a53f7
Rahul Madan <madan.rahul9@gmail.com>
2022-12-04 14:54:03 +0530

    updated search page added everything ':

--- src/MyComponents/SearchPage.js
+++ src/MyComponents/SearchPage.js
@@ -8,15 +8,19 @@ export const SearchPage = () => {
     const [question, setQuestion] = useState("")
     const [answers, setAnswers] = useState([])
     const [isSearching, setIsSearching] = useState(false)
+    const [detectedLang, setDetectedLang] = useState("")
+    const [checked, setChecked] = React.useState(false);
 
     const clickSearchButton = (e) => {
         e.preventDefault();
         setIsSearching(true)
         axios.post(process.env.REACT_APP_API_URI + process.env.REACT_APP_API_VERSION + "/get-answers", {
             'question': question,
-        }).then((res)=>{
+            'answer_in_english': checked.toString()
+        }).then((res) => {
             console.log(res.data.responses)
             setAnswers(res.data.responses)
+            setDetectedLang(res.data.source_lang)
             setIsSearching(false)
         })
     }
@@ -26,47 +30,59 @@ export const SearchPage = () => {
             <br/>
             <br/>
             <br/>
-            <div className="row justify-content-center">
-                <div className="col-12 col-md-10 col-lg-8">
-                    <form className="card card-sm">
-                        <div className="card-body row no-gutters align-items-center">
-                            <div className="col">
-                                <input className="form-control form-control-lg form-control-borderless"
-                                       placeholder="Search Question" onChange={(e) => {
-                                    setQuestion(e.target.value)
-                                }}/>
-                            </div>
-                            <div className="col-auto">
-                                {!isSearching?
-                                <button className="btn btn-lg btn-success" type="submit" onClick={clickSearchButton}>Search</button>
-                                :<button className="btn btn-lg btn-success" type="button" disabled>
-                                    <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"/>
-                                    Searching...
-                                </button>}
-                            </div>
-                        </div>
-                    </form>
+            <div className="mx-5">
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" onChange={() => {
+                        setChecked(!checked);
+                    }}/>
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Get Results in English
+                    </label>
                 </div>
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-10 col-lg-8">
+                        <form className="card card-sm">
+                            <div className="card-body row no-gutters align-items-center">
+                                <div className="col">
+                                    <input className="form-control form-control-lg form-control-borderless"
+                                           placeholder="Search Question" onChange={(e) => {
+                                        setQuestion(e.target.value)
+                                    }}/>
+                                </div>
+                                <div className="col-auto">
+                                    {!isSearching ?
+                                        <button className="btn btn-lg btn-success" type="submit"
+                                                onClick={clickSearchButton}>Search</button>
+                                        : <button className="btn btn-lg btn-success" type="button" disabled>
+                                            <span className="spinner-border spinner-border-sm" role="status"
+                                                  aria-hidden="true"/>
+                                            Searching...
+                                        </button>}
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <p>Total Answers Found: {answers.length}</p>
+                <p>Detected Language: {detectedLang}</p>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Retrieved Answers</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {answers.map((text, index) => {
+                        return <>
+                            <AnswerTableContent answer={text}/>
+                        </>
+                    })}
+                    </tbody>
+                </table>
+                <br/>
+                <br/>
+                <br/>
             </div>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">Retrieved Answers</th>
-                </tr>
-                </thead>
-                <tbody>
-                {answers.map((text,index) => {
-                    return <>
-                    <AnswerTableContent answer={text}/>
-                    </>
-                })}
-                </tbody>
-            </table>
-            <p>Total Answers Found: {answers.length}</p>
-            <br/>
-            <br/>
-            <br/>
-
         </>
     )
 }
